883d36b58e6cb6883a8015e07b22f0f491892001
Joe Aguado <aguado.joe@gmail.com>
2025-01-28 19:48:21 -0600

    project config, routes, folder structure

---
+++ constants/images.ts
@@ -0,0 +1,21 @@
+import onboarding from "@/assets/images/onboarding.png";
+import avatar from "@/assets/images/avatar.png";
+import house from "@/assets/images/house.png";
+import interior from "@/assets/images/interior.png";
+import cardGradient from "@/assets/images/card-gradient.png";
+import barChart from "@/assets/images/bar-chart.png";
+import whiteGradient from "@/assets/images/white-gradient.png";
+import map from "@/assets/images/map.png";
+import noResult from "@/assets/images/no-result.png";
+
+export default {
+  onboarding,
+  avatar,
+  house,
+  interior,
+  cardGradient,
+  barChart,
+  whiteGradient,
+  map,
+  noResult,
+};
